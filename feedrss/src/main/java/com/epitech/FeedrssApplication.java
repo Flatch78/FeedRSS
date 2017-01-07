825762784879038a749346a9d40a10ccd675c815
Guillaume Krier <guillaume.krier@epitech.eu>
2017-01-07 10:33:16 +0100

    Add first step FeedRSS project

---
+++ feedrss/src/main/java/com/epitech/FeedrssApplication.java
@@ -0,0 +1,12 @@
+package com.epitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedrssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedrssApplication.class, args);
+	}
+}
