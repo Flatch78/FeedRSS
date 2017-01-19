6da91a2bf399dfa3f785ae9d55ec1cee3ce1368c
leo.giroux <leo.giroux.epi@gmail.com>
2017-01-19 18:13:47 +0100

    add / route

--- src/main/java/Controllers/MainLauncher.java
+++ src/main/java/Controllers/MainLauncher.java
@@ -2,20 +2,23 @@ package Controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 /**
  * Created by leo on 19/01/2017.
  */
 
+@Controller
 @SpringBootApplication
 public class MainLauncher {
 
-
-    @RequestMapping("/helloword")
+    @ResponseBody
+    @RequestMapping("/")
     public String home() {
-        return "Hello World";
+        return "Hello mister guillaume this is a working API <3";
     }
 
     public static void main(String[] args) throws Exception {
