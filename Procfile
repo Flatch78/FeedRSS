3847623510b4d23ada925a38236fce07a8f03531
leo.giroux <leo.giroux.epi@gmail.com>
2017-01-19 18:06:20 +0100

    add mod Proc

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 web: java -Xmx384m -Xss512k -XX:+UseCompressedOops -jar target/*.jar 
 --spring.profiles.active=prod 
 --server.port=$PORT 
---spring.data.mongodb.uri=mongodb://leo:leo@ds117839.mlab.com:17839/heroku_57b5njl9
+--spring.data.mongodb.uri=${MONGOLAB_URI}
--- src/main/java/Controllers/MainLauncher.java
+++ src/main/java/Controllers/MainLauncher.java
@@ -2,6 +2,7 @@ package Controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 /**
@@ -10,6 +11,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MainLauncher {
+
+
+    @RequestMapping("/helloword")
+    public String home() {
+        return "Hello World";
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MainLauncher.class, args);
