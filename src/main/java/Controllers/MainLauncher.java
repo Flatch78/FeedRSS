cb43f1c8dc46ce3f86a7bfd3532ba4f933f5a272
leo.giroux <leo.giroux.epi@gmail.com>
2017-01-19 17:07:17 +0100

    first commit

---
+++ src/main/java/Controllers/MainLauncher.java
@@ -0,0 +1,17 @@
+package Controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Created by leo on 19/01/2017.
+ */
+
+@SpringBootApplication
+public class MainLauncher {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MainLauncher.class, args);
+    }
+}
