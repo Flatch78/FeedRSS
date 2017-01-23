828c9d0645b5b1d0c187331a71055436d6cc1290
edouard <edouard.marechal@epitech.eu>
2017-01-23 02:33:46 +0100

    User Auth v0.1

---
+++ src/main/java/com/feedRss/Controllers/UserController.java
@@ -0,0 +1,53 @@
+package com.feedRss.Controllers;
+
+import com.feedRss.Dao.UserRepository;
+import com.feedRss.Models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+/**
+ * Created by edouard on 22/01/17.
+ */
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET)
+    List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public User getUserById(@PathVariable("id") String id) {
+        return userRepository.findById(id);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteUserById(@PathVariable("id") String id) {
+        userRepository.delete(id);
+    }
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void addUser(@RequestBody User user) {
+        userRepository.insert(user);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/me", method = RequestMethod.GET)
+    public User getUser(@RequestHeader String id) {
+        return userRepository.findById(id);
+    }
+}
+
